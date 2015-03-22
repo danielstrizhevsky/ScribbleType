c4ce734fea76263ffe71d849ed6af92f6b7f98a2
Daniel Strizhevsky <danielstrizhevsky@gmail.com>
2015-03-21 19:39:09 -0500

    added dynamic spacing
    every letter is no longer the same size. also no longer writes every
    letter to a file

--- src/scribbleType.java
+++ src/scribbleType.java
@@ -1,7 +1,6 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -10,7 +9,11 @@ import javax.imageio.ImageIO;
 
 public class scribbleType {
 	
-	private static final double TEMPLATE_BORDER = 0.06;
+	private static final double TEMPLATE_BORDER = 0.08;
+	private static final int COLOR_THRESHOLD = 210; //higher means responds to lighter colors
+	private static final double PERCENTAGE_THRESHOLD = 0.03; //higher means it will need more dark pixels to stop trimming letters
+	private static final int KERNING = 5;
+	private static final int SPACE_LENGTH = 30;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedImage template = null;
@@ -20,16 +23,16 @@ public class scribbleType {
 		
 		BufferedImage[][] letters = splitLetters(template);
 		
-		for (int i = 0; i < letters.length; i++) {
+		/*for (int i = 0; i < letters.length; i++) {
 			for (int j = 0; j < letters[i].length; j++) {
 				String filename = ("letter" + i + "_" + j + ".jpg");
 				ImageIO.write(letters[i][j], "jpg", new File(filename));
 			}
-		}
+		}*/
 		
-		System.out.print("Enter width of canvas in characters: ");
+		System.out.print("Enter width of canvas in pixels: ");
 		int canvasWidth = scan.nextInt();
-		System.out.print("Enter height of canvas in characters: ");
+		System.out.print("Enter height of canvas in lines: ");
 		int canvasHeight = scan.nextInt();
 		scan.close();
 	
@@ -64,16 +67,12 @@ public class scribbleType {
 	
 	public static BufferedImage convertTextToImage(String filename,
 			int canvasWidth, int canvasHeight, BufferedImage[][] letters) 
-					throws FileNotFoundException {
-		
-		int letterWidth = letters[0][0].getWidth();
+					throws IOException {
 		int letterHeight = letters[0][0].getHeight();
 		int type = letters[0][0].getType();
-		BufferedImage scribble = new BufferedImage(canvasWidth * letterWidth,
-				canvasHeight * letterHeight, type);
+		BufferedImage scribble = new BufferedImage(canvasWidth, canvasHeight * letterHeight, type);
 		scribble.createGraphics().setPaint(Color.WHITE);
-		scribble.createGraphics().fillRect(0, 0, 
-				canvasWidth * letterWidth, canvasHeight * letterHeight);
+		scribble.createGraphics().fillRect(0, 0, canvasWidth, canvasHeight * letterHeight);
 		Scanner scan = new Scanner(new File(filename));
 		String text = "";
 		while (scan.hasNext()) {
@@ -83,6 +82,8 @@ public class scribbleType {
 			}
 		}
 		scan.close();
+		int prevPositionX = 0;
+		int currentPositionY = 0;
 		for (int i = 0; i < text.length(); i++) {
 			int letterIndex = 0;
 			int typeIndex = 0;
@@ -95,10 +96,47 @@ public class scribbleType {
 				typeIndex = (int) (Math.random() * 3) + 3;
 			}
 			if (Character.isAlphabetic(text.charAt(i))) {
-				scribble.createGraphics().drawImage(letters[letterIndex][typeIndex], null, 
-						(i % canvasWidth) * letterWidth, (i / canvasWidth) * letterHeight);
+				BufferedImage trimmedImage = trimWhiteSpace(letters[letterIndex][typeIndex], COLOR_THRESHOLD, PERCENTAGE_THRESHOLD);
+				int finalPositionX = prevPositionX + trimmedImage.getWidth();
+				if (finalPositionX > canvasWidth) {
+					prevPositionX = 0;
+					currentPositionY++;
+				}
+				scribble.createGraphics().drawImage(trimmedImage, null, 
+						prevPositionX, currentPositionY * letterHeight);
+				prevPositionX += trimmedImage.getWidth() + KERNING;
+			}
+			else {
+				prevPositionX += SPACE_LENGTH;
 			}
 		}
 		return scribble;
 	}
+	
+	//threshold of average r/g/b value, weightThreshold percentage of a "scan"
+	//that is colored in (non-blank)
+	public static BufferedImage trimWhiteSpace(BufferedImage letter, int threshold, double weightThreshold) throws IOException {
+		int left = 0;
+		int right = letter.getWidth();
+		int[] colorWeights = new int[right];
+		for (int i = 0; i < letter.getWidth(); i++) {
+			for (int j = 0; j < letter.getHeight(); j++) {
+				Color color = new Color(letter.getRGB(i, j));
+				if ((color.getRed() + color.getGreen() + color.getBlue()) / 3 < threshold) {
+					colorWeights[i]++; //if the color is past the threshold add to weight
+				}
+			}
+		}
+		for (int i = 0; i < letter.getWidth() - 1 && 
+				((double)colorWeights[i] / letter.getHeight()) < weightThreshold; i++) {
+			left++;
+		}
+		for (int i = letter.getWidth() - 1; right > left + 1 && 
+				((double) colorWeights[i] / letter.getHeight()) < weightThreshold; i--) {
+			right--;
+		}
+		//ImageIO.write(letter.getSubimage(left, 0, right - left, letter.getHeight()), "jpg", new File("letter" + right + "_" + left + ".jpg"));
+		return letter.getSubimage(left, 0, right - left, letter.getHeight());
+	}
+	
 }
